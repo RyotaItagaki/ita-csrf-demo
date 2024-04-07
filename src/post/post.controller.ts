f93f871923bf2454363b26d5c9bdb57017e6b533
Ryota Itagaki <ribitamana@gmail.com>
2024-04-07 11:36:10 +0900

    feat: 投稿一覧API

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,10 +1,15 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { PostService } from './post.service';
-import { CreatePostDto } from './CreatePostDto';
+import { CreatePostDto } from './Dto';
 
 @Controller('post')
 export class PostController {
   constructor(private readonly postService: PostService) {}
+  // 投稿一覧を取得
+  @Get('list')
+  async listPosts() {
+    return await this.postService.listPosts();
+  }
 
   // 投稿を作成
   @Post('create')
--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@nestjs/common';
 import { prisma } from '../../prisma/client';
-import { CreatePostDto } from './CreatePostDto';
+import { CreatePostDto } from './Dto';
 
 @Injectable()
 export class PostService {
+  // 投稿一覧取得
+  async listPosts() {
+    const posts = await prisma.post.findMany({
+      include: {
+        user: true,
+      },
+    });
+
+    return posts.map((post) => {
+      return {
+        id: post.id,
+        text: post.text,
+        createdAt: post.createdAt,
+        user: {
+          id: post.user.id,
+          name: post.user.name,
+        },
+      };
+    });
+  }
+
+  // 投稿作成
   async createPost(post: CreatePostDto) {
     // 分割代入
     const { userId, text } = post;
